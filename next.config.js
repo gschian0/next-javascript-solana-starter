ce2a91d993e0b4961730180cf1b5a1d711d78501
schianoresearch <gschiano@me.com>
2023-02-24 23:49:08 -0500

    first working with ai and block!

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "replicate.com",
+      },
+      {
+        protocol: "https",
+        hostname: "replicate.delivery",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,11 @@
 import dynamic from "next/dynamic";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { useState } from "react";
+import Head from "next/head";
+
+
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 // Default styles that can be overridden by your app
 require("@solana/wallet-adapter-react-ui/styles.css");
@@ -11,6 +16,42 @@ const WalletMultiButtonDynamic = dynamic(
   { ssr: false }
 );
 const Home = () => {
+  const [prediction, setPrediction] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("/api/predictions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: e.target.prompt.value,
+      }),
+    });
+    let prediction = await response.json();
+    if (response.status !== 201) {
+      setError(prediction.detail);
+      return;
+    }
+    setPrediction(prediction);
+
+    while (
+      prediction.status !== "succeeded" &&
+      prediction.status !== "failed"
+    ) {
+      await sleep(1000);
+      const response = await fetch("/api/predictions/" + prediction.id);
+      prediction = await response.json();
+      if (response.status !== 200) {
+        setError(prediction.detail);
+        return;
+      }
+      console.log({prediction})
+      setPrediction(prediction);
+    }
+  };
   // Here's how to get the thirdweb SDK instance
   // const sdk = useSDK();
   // Here's how to get a nft collection
@@ -20,7 +61,7 @@ const Home = () => {
   // );
 
   return (
-    <>
+    
       <div className={styles.container}>
         <div className={styles.iconContainer}>
           <Image
@@ -57,9 +98,32 @@ const Home = () => {
         </p>
 
         <WalletMultiButtonDynamic />
-      </div>
-    </>
+        <p>
+        Dream something with{" "}
+        <a href="https://replicate.com/stability-ai/stable-diffusion">stability-ai/stable-diffusion</a>:
+      </p>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input type="text" name="prompt" placeholder="Enter a prompt to display an image" />
+        <button type="submit">Go!</button>
+      </form>
+      {error && <div>{error}</div>}     
+      {prediction && (
+        <div>
+            {prediction.output && (
+              <div className={styles.imageWrapper}>
+              <Image
+                fill
+                src={prediction.output[prediction.output.length - 1]}
+                alt="output"
+                sizes='50vw'
+                style={{ maxWidth: '75vw', maxHeight: '75vw' }}
+              />
+              </div>
+            )}  
+            <p>status: {prediction.status}</p>
+        </div>
+      )}
+    </div>
   );
-};
-
+}
 export default Home;
